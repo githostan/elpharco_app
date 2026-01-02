73a36e416b817272fa6605090bbe3a1f53fc7d8c
Stanley Nwachukwu <stanleyc.nwachukwu@gmail.com>
2026-01-02 14:59:07 +0000

    modified modules/network/main.tf to create eip resource for the network module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,5 +1,7 @@
 
-### VPC #################################################################
+# the vpc provides an isolated, customizable network environment that hosts all tiers of the application securely.
+
+### vpc #################################################################
 resource "aws_vpc" "arco_infra" {
   cidr_block = var.vpc_cidr
 
@@ -11,11 +13,13 @@ resource "aws_vpc" "arco_infra" {
 ###############################################################################################################################################################################
 ###############################################################################################################################################################################
 
-### public subnet1 ########################################################
+# public subnets allow resources to have direct internet access through the internet gateway.
+
+### public subnet_01 ########################################################
 resource "aws_subnet" "arco_pub_subnet_01" {
   vpc_id     = aws_vpc.arco_infra.id
   cidr_block = var.pub_subnet_01_cidr
-  map_public_ip_on_launch = true  # Enable auto-assigning public IPv4 addresses
+  map_public_ip_on_launch = true  # enable auto-assigning public ipv4 addresses
   availability_zone       = var.pub_subnet_01_az  # Specify the availability zone
 
   tags = {
@@ -23,12 +27,12 @@ resource "aws_subnet" "arco_pub_subnet_01" {
   }
 }
 
-### public subnet2 ########################################################
+### public subnet_02 ########################################################
 resource "aws_subnet" "arco_pub_subnet_02" {
   vpc_id     = aws_vpc.arco_infra.id
   cidr_block = var.pub_subnet_02_cidr
-  map_public_ip_on_launch = true  # Enable auto-assigning public IPv4 addresses
-  availability_zone       = var.pub_subnet_02_az  # Specify the availability zone
+  map_public_ip_on_launch = true  # enable auto-assigning public ipv4 addresses
+  availability_zone       = var.pub_subnet_02_az  # specify the availability zone
 
   tags = {
     Name = "pub_subnet_02"
@@ -37,24 +41,27 @@ resource "aws_subnet" "arco_pub_subnet_02" {
 ###############################################################################################################################################################################
 ###############################################################################################################################################################################
 
-## private subnet1 ########################################################
+# private subnets isolate app and db resources from the public internet while still allowing outbound access 
+# via the nat gateway.
+
+## private subnet_01 ########################################################
 resource "aws_subnet" "arco_prvt_subnet_01" {
   vpc_id     = aws_vpc.arco_infra.id
   cidr_block = var.prvt_subnet_01_cidr
-  map_public_ip_on_launch = false  # Enable auto-assigning public IPv4 addresses
-  availability_zone       = var.prvt_subnet_01_az # Specify the availability zone
+  map_public_ip_on_launch = false  # enable auto-assigning public ipv4 addresses
+  availability_zone       = var.prvt_subnet_01_az # specify the availability zone
 
   tags = {
     Name = "prvt_subnet_01"
   }
 }
 
-## private subnet2 ########################################################
+## private subnet_02 ########################################################
 resource "aws_subnet" "arco_app_prvt_subnet_02" {
   vpc_id     = aws_vpc.arco_infra.id
   cidr_block = var.prvt_subnet_02_cidr
-  map_public_ip_on_launch = false  # Enable auto-assigning public IPv4 addresses
-  availability_zone       = var.prvt_subnet_02_az   # Specify the availability zone
+  map_public_ip_on_launch = false  # enable auto-assigning public ipv4 addresses
+  availability_zone       = var.prvt_subnet_02_az   # specify the availability zone
 
   tags = {
     Name = "prvt_subnet_02"
@@ -64,7 +71,10 @@ resource "aws_subnet" "arco_app_prvt_subnet_02" {
 ###############################################################################################################################################################################
 ###############################################################################################################################################################################
 
-### Internet Gateway for the public subnets ###
+# internet gateway provides the vpc with a path to the public internet, enabling public subnets and the nat gateway 
+# to send outbound traffic.
+
+### internet gateway for the public subnets ###
 resource "aws_internet_gateway" "arco_intgw" {
   vpc_id = aws_vpc.arco_infra.id
 
@@ -76,3 +86,16 @@ resource "aws_internet_gateway" "arco_intgw" {
 ###############################################################################################################################################################################
 ###############################################################################################################################################################################
 
+# elastic ip provides a fixed, public ipv4 address that the nat Gateway uses for all outbound internet traffic.
+# the nat gateway must have a public or elastic ip to reach the internet through the internet gateway.
+# Creating this eip ensures the nat gateway has a stable, routable address so private‑subnet
+# resources (app/db tiers) can access the internet securely without being exposed.
+
+## elastic ip for nat-gateway #############
+resource "aws_eip" "arco_eip" {
+  domain = "vpc"
+  #depends_on = [aws_internet_gateway.arco_ingw]
+  tags = {
+    Name = "arco_eip"
+  }
+}      
