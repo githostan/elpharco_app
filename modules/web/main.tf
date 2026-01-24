932cf6ee72995133e883b367deb68f5ed08c59d8
Stanley Nwachukwu <stanleyc.nwachukwu@gmail.com>
2026-01-24 21:50:13 +0000

    feat(web): created web_asg & target-tracking policy

--- modules/web/main.tf
+++ modules/web/main.tf
@@ -20,7 +20,7 @@ resource "aws_launch_template" "arco_web" {
     enabled = true
   }
 
-# We can't modify a launch configuration, so any changes to the definition force terraform 
+# we can't modify a launch configuration, so any changes to the definition force terraform 
 # to create a new resource. The create_before_destroy argument in the lifecycle block instructs 
 # terraform to create the new version before destroying the original to avoid any service interruptions.
 # lifecycle {
@@ -40,7 +40,7 @@ resource "aws_security_group" "arco_web_sg" {
   description = "allow ssh on port 22 & http on port 80"  # Security group for instances launched by the launch template
   vpc_id      = var.vpc_id
 
-// Define inbound and outbound rules as needed
+// define inbound and outbound rules as needed
   ingress {
     from_port        = 22
     to_port          = 22
@@ -56,7 +56,7 @@ resource "aws_security_group" "arco_web_sg" {
     #security_groups  = [aws_security_group.arco_web_alb_sg.id]
   }
 
-# Outbound Rules
+# outbound rules
   egress {
     from_port        = 0
     to_port          = 0
@@ -67,3 +67,59 @@ resource "aws_security_group" "arco_web_sg" {
     Name = "web_sg"
   }
 }
+######################################################################################################################################################
+#######################################################################################################################################################
+
+# auto scaling group for the web-tier frontend servers
+# automatically launches and terminates ec2 instances across multiple AZs, using the public subnets provided by the network module.
+# ensures high availability by distributing instances across public subnets and scaling capacity based on demand.
+resource "aws_autoscaling_group" "arco_web_asg" {
+  name_prefix = "web_asg"
+  #availability_zones = ["us-east-1a", "us-east-1b"]
+  desired_capacity   = 2
+  max_size           = 5
+  min_size           = 2
+  vpc_zone_identifier = [aws_subnet.arco_pub_subnet_01.id, aws_subnet.arco_pub_subnet_02.id]  # public subnet ids
+  target_group_arns = [aws_lb_target_group.arco_web_target_grp_alb.arn]
+  health_check_type = "ELB"
+  health_check_grace_period = 300  # Optional but recommended
+
+  launch_template {
+    id      = aws_launch_template.arco_web.id
+    version = "$Latest"
+  }
+
+# When replacing a resource, terraform would normally delete the old resource first and then create the replacement.
+# However, as ASG now has a reference to the old resource, Terraform will not be able to delete it, and as such, we use 
+# the lifecycle setting called “Create before destroy” to force terraform to create the replacement resource first prior to deleting the old resource
+ lifecycle { 
+     create_before_destroy = true
+   }
+
+   tag {
+    key                 = "Name"
+    value               = "web_instance"
+    propagate_at_launch = true
+  }
+}
+
+# target-tracking scaling policy for the web asg.
+# adjusts capacity automatically based on average cpu utilization
+# ensures the asg scales out/in to maintain the desired cpu threshold
+# maintains performance by adjusting instance count according to load
+resource "aws_autoscaling_policy" "web_cpu_policy" {
+  name                   = "cpu_scaling_policy"
+  policy_type            = "targetTrackingScaling"
+  autoscaling_group_name = aws_autoscaling_group.arco_web_asg.name
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 90.0  # target cpu utilization
+  }
+}
+
+######################################################################################################################################################
+#######################################################################################################################################################
